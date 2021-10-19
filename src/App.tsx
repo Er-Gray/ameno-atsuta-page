0942efa32074e5fa76c5232314550fe8c4f65067
Er-Gray <sakuya.mybride@gmail.com>
2021-10-19 16:49:10 +0900

    ページ作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+import styled from "styled-components";
+import Home from "./Home";
+import About from "./About";
+import NotFound from "./NotFound";
 
-function App() {
+const App:React.FC=()=>{
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from "styled-components";
+
+const Home:React.FC=()=>{
+	return(
+		<div>
+
+		</div>
+	);
+};
+
+export default Home;

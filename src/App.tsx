9dd6ef643f3d50cc24a3e48671302f4fabf7f152
Er-Gray <sakuya.mybride@gmail.com>
2021-10-28 16:52:52 +0900

    headerの追加とリセットcssの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./destyle.css";
 import {
   BrowserRouter,
   Switch,
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
 import styled from "styled-components";
 
 const HeaderWrapper=styled.div`
@@ -16,25 +17,46 @@ const AmenoLogo=styled.img`
 `;
 
 const HeaderRight=styled.div`
-
+	display:flex;
+	justify-content:flex-end;
 `;
 
 const HeaderUl=styled.ul`
 	display:flex;
 	flex-wrap:nowrap;
+	width:40%;
+	list-style:none;
 `;
 
 const HeaderLi=styled.li`
+	width:25%;
+	height:100%;
+`;
 
+const HeaderYTAndTw=styled.ul`
+	width:15%;
+`;
+
+const HeaderYTAndTwLi=styled.li`
+	width:100%;
 `;
 
 const Header:React.FC=()=>{
 	const HeaderMenuList:React.FC=()=>{
 
 		return (
-			<HeaderUl>
-				<HeaderLi>ABOUT</HeaderLi>
-			</HeaderUl>
+			<BrowserRouter>
+				<HeaderUl>
+					<HeaderLi><Link to="/about">ABOUT</Link></HeaderLi>
+					<Link to="/stream"><HeaderLi>STREAM</HeaderLi></Link>
+					<Link to="/history"><HeaderLi>HISTORY</HeaderLi></Link>
+					<Link to="/contact"><HeaderLi>CONTACT</HeaderLi></Link>
+				</HeaderUl>
+				<HeaderYTAndTw>
+					<HeaderYTAndTwLi>Youtube</HeaderYTAndTwLi>
+					<HeaderYTAndTwLi>Twitter</HeaderYTAndTwLi>
+				</HeaderYTAndTw>
+			</BrowserRouter>
 		);
 	}
 
